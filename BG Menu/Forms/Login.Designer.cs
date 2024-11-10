be99deca816adcf3019e0870b662344d7fb52333
Elliot Renner <Bananagoat@hotmail.co.uk>
2024-11-10 12:40:54 +0000

    Nothing to Note

--- BG Menu/Forms/Login.Designer.cs
+++ BG Menu/Forms/Login.Designer.cs
@@ -51,9 +51,10 @@
             loginButton.BackColor = Color.FromArgb(46, 51, 73);
             loginButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             loginButton.ForeColor = SystemColors.Control;
-            loginButton.Location = new Point(53, 477);
+            loginButton.Location = new Point(76, 795);
+            loginButton.Margin = new Padding(4, 5, 4, 5);
             loginButton.Name = "loginButton";
-            loginButton.Size = new Size(166, 52);
+            loginButton.Size = new Size(237, 87);
             loginButton.TabIndex = 0;
             loginButton.Text = "Login";
             loginButton.UseVisualStyleBackColor = false;
@@ -65,9 +66,10 @@
             usernameTextBox.BorderStyle = BorderStyle.None;
             usernameTextBox.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold);
             usernameTextBox.ForeColor = SystemColors.Info;
-            usernameTextBox.Location = new Point(99, 360);
+            usernameTextBox.Location = new Point(141, 600);
+            usernameTextBox.Margin = new Padding(4, 5, 4, 5);
             usernameTextBox.Name = "usernameTextBox";
-            usernameTextBox.Size = new Size(296, 28);
+            usernameTextBox.Size = new Size(423, 42);
             usernameTextBox.TabIndex = 1;
             // 
             // passwordTextBox
@@ -76,9 +78,10 @@
             passwordTextBox.BorderStyle = BorderStyle.None;
             passwordTextBox.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold);
             passwordTextBox.ForeColor = SystemColors.Info;
-            passwordTextBox.Location = new Point(99, 415);
+            passwordTextBox.Location = new Point(141, 692);
+            passwordTextBox.Margin = new Padding(4, 5, 4, 5);
             passwordTextBox.Name = "passwordTextBox";
-            passwordTextBox.Size = new Size(263, 28);
+            passwordTextBox.Size = new Size(376, 42);
             passwordTextBox.TabIndex = 2;
             passwordTextBox.UseSystemPasswordChar = true;
             // 
@@ -87,9 +90,10 @@
             SetPasswordButton.BackColor = Color.FromArgb(46, 51, 73);
             SetPasswordButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             SetPasswordButton.ForeColor = SystemColors.Control;
-            SetPasswordButton.Location = new Point(229, 477);
+            SetPasswordButton.Location = new Point(327, 795);
+            SetPasswordButton.Margin = new Padding(4, 5, 4, 5);
             SetPasswordButton.Name = "SetPasswordButton";
-            SetPasswordButton.Size = new Size(166, 52);
+            SetPasswordButton.Size = new Size(237, 87);
             SetPasswordButton.TabIndex = 3;
             SetPasswordButton.Text = "Exit";
             SetPasswordButton.UseVisualStyleBackColor = false;
@@ -98,9 +102,10 @@
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources.Password;
-            pictureBox2.Location = new Point(53, 408);
+            pictureBox2.Location = new Point(76, 680);
+            pictureBox2.Margin = new Padding(4, 5, 4, 5);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(40, 40);
+            pictureBox2.Size = new Size(57, 67);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.TabIndex = 12;
             pictureBox2.TabStop = false;
@@ -108,9 +113,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.User;
-            pictureBox1.Location = new Point(53, 353);
+            pictureBox1.Location = new Point(76, 588);
+            pictureBox1.Margin = new Padding(4, 5, 4, 5);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(40, 40);
+            pictureBox1.Size = new Size(57, 67);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 11;
             pictureBox1.TabStop = false;
@@ -118,25 +124,28 @@
             // panel3
             // 
             panel3.BackColor = Color.White;
-            panel3.Location = new Point(99, 391);
+            panel3.Location = new Point(141, 652);
+            panel3.Margin = new Padding(4, 5, 4, 5);
             panel3.Name = "panel3";
-            panel3.Size = new Size(296, 1);
+            panel3.Size = new Size(423, 2);
             panel3.TabIndex = 13;
             // 
             // panel1
             // 
             panel1.BackColor = Color.White;
-            panel1.Location = new Point(99, 447);
+            panel1.Location = new Point(141, 745);
+            panel1.Margin = new Padding(4, 5, 4, 5);
             panel1.Name = "panel1";
-            panel1.Size = new Size(296, 1);
+            panel1.Size = new Size(423, 2);
             panel1.TabIndex = 14;
             // 
             // pictureBox3
             // 
             pictureBox3.Image = Properties.Resources.Eye;
-            pictureBox3.Location = new Point(367, 415);
+            pictureBox3.Location = new Point(524, 692);
+            pictureBox3.Margin = new Padding(4, 5, 4, 5);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(28, 28);
+            pictureBox3.Size = new Size(40, 47);
             pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox3.TabIndex = 15;
             pictureBox3.TabStop = false;
@@ -145,20 +154,22 @@
             // 
             label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ControlLightLight;
-            label1.Location = new Point(12, 551);
+            label1.Location = new Point(17, 918);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(434, 37);
+            label1.Size = new Size(620, 62);
             label1.TabIndex = 16;
-            label1.Text = "Version : 2.5.0";
+            label1.Text = "Version : 2.5.1";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // hoVpnStatusLabel
             // 
             hoVpnStatusLabel.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             hoVpnStatusLabel.ForeColor = Color.FromArgb(46, 51, 73);
-            hoVpnStatusLabel.Location = new Point(11, 18);
+            hoVpnStatusLabel.Location = new Point(16, 30);
+            hoVpnStatusLabel.Margin = new Padding(4, 0, 4, 0);
             hoVpnStatusLabel.Name = "hoVpnStatusLabel";
-            hoVpnStatusLabel.Size = new Size(434, 34);
+            hoVpnStatusLabel.Size = new Size(620, 57);
             hoVpnStatusLabel.TabIndex = 17;
             hoVpnStatusLabel.Text = "label2";
             hoVpnStatusLabel.TextAlign = ContentAlignment.MiddleCenter;
@@ -167,19 +178,20 @@
             // 
             sapVpnStatusLabel.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             sapVpnStatusLabel.ForeColor = Color.FromArgb(46, 51, 73);
-            sapVpnStatusLabel.Location = new Point(11, 52);
+            sapVpnStatusLabel.Location = new Point(16, 87);
+            sapVpnStatusLabel.Margin = new Padding(4, 0, 4, 0);
             sapVpnStatusLabel.Name = "sapVpnStatusLabel";
-            sapVpnStatusLabel.Size = new Size(434, 34);
+            sapVpnStatusLabel.Size = new Size(620, 57);
             sapVpnStatusLabel.TabIndex = 18;
             sapVpnStatusLabel.Text = "label2";
             sapVpnStatusLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Login
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(46, 51, 73);
-            ClientSize = new Size(458, 611);
+            ClientSize = new Size(654, 1018);
             Controls.Add(sapVpnStatusLabel);
             Controls.Add(hoVpnStatusLabel);
             Controls.Add(label1);
@@ -194,6 +206,7 @@
             Controls.Add(loginButton);
             FormBorderStyle = FormBorderStyle.None;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Login";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Login";

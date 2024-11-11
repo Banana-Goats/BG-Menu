f676c629f1c39c13b98360244dfbbc68989f5427
Elliot Renner <Bananagoat@hotmail.co.uk>
2024-11-11 10:55:46 +0000

    Button renamed

--- BG Menu/Forms/Sub Forms/EmailManager.Designer.cs
+++ BG Menu/Forms/Sub Forms/EmailManager.Designer.cs
@@ -59,7 +59,7 @@
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 2;
-            button1.Text = "button1";
+            button1.Text = "Export";
             button1.UseVisualStyleBackColor = true;
             // 
             // EmailManager
